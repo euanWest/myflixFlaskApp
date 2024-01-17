5a3562a588516546d7144c8b5278410fc4437181
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-17 14:47:15 +0000

    fhuagojvidorhid

--- myFlixupdater.py
+++ myFlixupdater.py
@@ -2,6 +2,7 @@ import subprocess
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/Update")
 def updateCheck():
@@ -13,5 +14,5 @@ def updateCheck():
      # IF Change in github repo
     return "<p> Updating sever</p>"
 
-if __name__ == '__app__':
-    app.run(debug=True, port=80)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80)
