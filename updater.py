d0c7da6230e7ef4fc942c8c0c0e53dc3e1ced7ef
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-17 12:51:15 +0000

    bxbngf

--- updater.py
+++ updater.py
@@ -1,12 +1,6 @@
 import subprocess
 from flask import Flask
-# app = Flask(__name__)
-# app.debug = True
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=80)
-
-# instance of flask application
 app = Flask(__name__)
 
 @app.route("/Update")
@@ -19,14 +13,5 @@ def updateCheck():
      # IF Change in github repo
     return "<p> Updating sever</p>"
 
- 
-
- 
-# home route that returns below text 
-# when root url is accessed
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
- 
 if __name__ == '__app__':
-    app.run(debug=True, port=8001)
+    app.run(debug=True, port=80)
