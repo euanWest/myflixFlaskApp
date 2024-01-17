5c5bdac94e49f0ef3714f4740c506f3e45b2d83c
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-17 00:36:59 +0000

    added test to update page

--- updater.py
+++ updater.py
@@ -6,6 +6,7 @@ app.debug = True
 @app.route("/Update")
 def updateCheck():
     # IF Change in github repo
+    return "<p> Updating sever</p>"
     subprocess.run(['git fetch'])
     subprocess.run(['git pull'])
     subprocess.run(['sudo bash Buildfile'])
