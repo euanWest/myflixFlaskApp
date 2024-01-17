b4cf8b2c5e8f5c6b2beec3f44f2a129ffb7e86b1
euanWest <72396471+euanWest@users.noreply.github.com>
2024-01-17 11:49:30 +0000

    bedugging

--- updater.py
+++ updater.py
@@ -3,10 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route("/Update")
 def updateCheck():
-    # IF Change in github repo
-    return "<p> Updating sever</p>"
+   
     subprocess.run(['git fetch'])
     subprocess.run(['git pull'])
     subprocess.run(['sudo bash Buildfile'])
+
+     # IF Change in github repo
+    return "<p> Updating sever</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port="80")
